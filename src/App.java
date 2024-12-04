40026d01ec89431f19272d617f2caf72f433f788
John_Tigre <cuji309@gmail.com>
2024-12-04 13:01:12 -0500

    Se finalizo el proyecto

--- src/App.java
+++ src/App.java
@@ -3,7 +3,7 @@ import Materia.Stacks.Stack;
 public class App {
     public static void main(String[] args) throws Exception {
         runStack();
-        
+
     }
 
     public static void runStack() {
@@ -16,6 +16,15 @@ public class App {
         stack.push(7);
         stack.push(12);
         stack.push(23);
+
+        // Imprimir la pila usando printStack
+        System.out.println("Contenido de la pila:");
+        stack.printStack();
+        System.out.println(""); // Esto imprimira directamente los elementos de la pila
+
+        // Obtener y mostrar el tamaño de la pila
+        System.out.println("Tamaño de la pila: " + stack.getSize());
+
 
         //Mostrar elementos de la pila
         System.out.println("Cima   -> "+ stack.peek());
